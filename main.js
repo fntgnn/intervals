f65510f986af7e4c66b3db37079f186042aa6ac6
Giovanni <gio.fon@gmail.com>
2022-09-08 12:43:08 +0200

    trying install button

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+
 $(document).ready(function() {
     if (navigator && navigator.serviceWorker) {
         navigator.serviceWorker.register('sw.js');
     }
+
+    //install button
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+        alert(e);
+    });
+    const installApp = document.getElementById('installApp');
+
+    installApp.addEventListener('click', async () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                deferredPrompt = null;
+            }
+        }
+    });
+
+
     $.ajax({
         type: "GET",
         url: "intervals.csv",
